c4e48f7ca1b102c6ba785f2b00985f95e8fec9f0
Taoufik <taoufik.almejri@gmail.com>
2020-02-19 09:45:53 +0100

    patch require_admin_or_api_request method

--- lib/redmine_partial_admin/application_controller_patch.rb
+++ lib/redmine_partial_admin/application_controller_patch.rb
@@ -7,6 +7,9 @@ module RedminePartialAdmin
       base.class_eval do
         alias_method :require_admin_without_partial_admin, :require_admin
         alias_method :require_admin, :require_admin_with_partial_admin
+
+        alias_method :require_admin_or_api_request_without_partial_admin, :require_admin_or_api_request
+        alias_method :require_admin_or_api_request, :require_admin_or_api_request_with_partial_admin
       end
     end
 
@@ -18,6 +21,17 @@ module RedminePartialAdmin
           return false
         end
         true
+      end
+
+      def require_admin_or_api_request_with_partial_admin
+        return true if api_request?
+        if User.current.admin? || User.current.partial_admin?
+          true
+        elsif User.current.logged?
+          render_error(:status => 406)
+        else
+          deny_access
+        end
       end
     end
   end
